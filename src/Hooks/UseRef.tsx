f84497a14752f32adbd490a9c5fbc62ed275c18f
Purabsingla <purabsingla15@gmail.com>
2025-02-09 16:51:45 +0530

    understanding useRef

--- src/Hooks/UseRef.tsx
+++ src/Hooks/UseRef.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const UseRef: React.FC = () => {
+  const Ref = useRef<HTMLInputElement | null>(null);
   return (
     <div>
       <h1 className="font-extrabold text-4xl">UseRef</h1>
+      <h2 className="text-2xl">
+        There is no major use of useRef we can scrool to a component or div
+        element or set focus to a input element or increment or decrement valuse
+        withot re render and so on{" "}
+      </h2>
+      <input type="text" placeholder="something....." ref={Ref} />
+      <button onClick={() => Ref.current?.focus()}>Set Focus Input</button>
     </div>
   );
 };
